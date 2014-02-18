4bfeee93a35089686d526e3a0f79558c59801ca0
Dominik Macháček <gldksl@atrey.karlin.mff.cuni.cz>
2014-02-18 21:56:15 +0100

    sekej znova
    jmenuje se sek a docela funguje, tu původní smažu
    
    v návrhu nové značení

--- sekacek.py
+++ sekacek.py
@@ -1,15 +1,16 @@
 import re
-def masky(slovo):
+def rozliš(slovo):
 	slovo=slovo.lower()
 	for i in range(1,len(slovo)):
 		x=slovo[i-1]	
 		b=slovo[i-1:]
-		b=re.sub(x+x,x+'0',b)
+		b=re.sub(x+x,x+'0',b) #jde to zrychlit, tohle je O(N^2)
 		slovo=slovo[:i-1]+b
+
 	slovo=re.sub('ch','c0',slovo)
 	slovo=re.sub('s(t(?!(r|ř|n|l))|p)','s0',slovo)
 	slovo=re.sub('th','t0',slovo)
-	print(slovo)
+
 	slovo=re.sub('a(u|e)','a0',slovo)
 	slovo=re.sub('e(u|i)','e0',slovo)
 	slovo=re.sub('o(u|i)','o0',slovo)
@@ -38,11 +39,49 @@ def masky(slovo):
 
 	slovo=re.sub('(l|s|ř|v|r)','K',slovo) # zbytek
 
-
-
 	return slovo
 
-print(masky('vichr bystřina břicho bysta přeskvělý koniklec pekl'))
-print(masky('postla'))
-print(masky('skoro Anna lopata jak se Dominik Macháček Máte!šč'))
+def sekej(slovo):
+	maska=rozliš(slovo)
+	s=''
+	vystup=''
+	j=0
+	for i in range(len(slovo)):
+		s=s+maska[i]
+		print(s)
+		if re.match('K?K0*V',s):
+			vystup=vystup+'/'+slovo[j:j+len(s)]
+			j+=len(s)
+			s=''
+	print(vystup,maska)
+
+#pravidla budou:
+#1 začíná se V?
+#2 KV
+def sek(slovo):
+	maska=rozliš(slovo)
+	z=''
+	if re.search(r'^VK[^VK]*K',maska):
+		z=re.sub(r'(^VK)(.*$)',r'\1',maska)
+		maska=re.sub(r'(^VK)(.*$)',r'\2',maska)
+	elif re.search(r'^VK0*[^K]',maska):
+		z='V'	
+		maska=maska[1:]
+	#chci, aby to V odřízlo i s K, pokud má být za ním, a dalo to do z, to se pak přilepí na začátek
+	maska=re.sub(r'(K[^V]*V(K$)?)',r'\1/',maska)
+	maska=((z+'/') if z else '') + maska
+	print(maska)
+#sekej('krok')
+	
+#print(masky('vichr bystřina břicho bysta přeskvělý koniklec pekl'))
+sek('postla')
+sek('skoro')
+sek('Anna')
+sek('Antonín')
+sek('lopata')
+sek('postavit')
+# jak se Dominik Macháček Máte!šč'))
+
+#asi bude lepší ou měnit za 0V, ne V0
+
 
